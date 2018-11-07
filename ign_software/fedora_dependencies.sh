af96589991e9b7b87d355b8b6d8e77ac8d857fca
Edgar Granados Osegueda <edgar.granados@itam.mx>
2018-11-07 12:50:48 -0600

    updated pkgs names and permissions

--- ign_software/fedora_dependencies.sh
+++ ign_software/fedora_dependencies.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+set -e
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+dnf install -y mercurial doxygen cppcheck # global
+dnf install -y rubygem-ronn ruby-devel # ign-transport 
+dnf install -y protobuf protobuf-devel protobuf-compiler # ign-msgs
--- ign_software/ubuntu_dependencies.sh
+++ ign_software/ubuntu_dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 apt install -y mercurial doxygen cppcheck # global
 apt install -y ruby-ronn ruby-dev # ign-transport 
 apt install -y libprotobuf-dev protobuf-compiler libprotoc-dev # ign-msgs
